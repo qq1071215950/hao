34292d80e5fa89e1538f32d4826a96ee6823f454
root123 <1071215950@qq.com>
2019-07-08 19:02:16 +0800

    add;添加用户实体类

---
+++ src/main/java/com/example/hao/pojo/User.java
@@ -0,0 +1,40 @@
+package com.example.hao.pojo;
+
+public class User {
+    private String userName;
+    private String password;
+    private String phone;
+    private String idCart;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getIdCart() {
+        return idCart;
+    }
+
+    public void setIdCart(String idCart) {
+        this.idCart = idCart;
+    }
+}
